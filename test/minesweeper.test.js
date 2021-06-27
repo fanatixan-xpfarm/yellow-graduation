d3ff010503d236378a7873cc9a2bf31f57f95a98
Attila-Fejer <attila.fejer@ibm.com>
2021-06-27 20:50:13 +0200

    refactor(US2/UAT2.1): game constructor always receives a board

--- test/minesweeper.test.js
+++ test/minesweeper.test.js
@@ -4,7 +4,11 @@ describe('Minesweeper', () => {
   describe('US1 - Starting new game', () => {
     test('GIVEN a new game WHEN printing board THEN the result should be \n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+', () => {
       // given
-      const minesweeper = new Minesweeper();
+      const minesweeper = new Minesweeper([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+      ]);
 
       // when
       const board = minesweeper.print();
