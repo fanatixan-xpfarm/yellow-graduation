735926220e50ac222ca036fd942c1681bcb31198
Attila-Fejer <attila.fejer@ibm.com>
2021-06-27 20:46:29 +0200

    refactor(US2/UAT2.1): extracted row printing

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -18,17 +18,20 @@ class Minesweeper {
       ROW_SEPARATOR +
       NEW_LINE +
       this.board
-        .map(
-          (row) =>
-            SQUARE_SEPARATOR +
-            row
-              .map((square) => (square === 1 ? BOMB : CLOSED))
-              .join(SQUARE_SEPARATOR) +
-            SQUARE_SEPARATOR
-        )
+        .map((row) => this.printRow(row))
         .join(NEW_LINE + ROW_SEPARATOR + NEW_LINE) +
       NEW_LINE +
       ROW_SEPARATOR
+    );
+  }
+
+  printRow(row) {
+    return (
+      SQUARE_SEPARATOR +
+      row
+        .map((square) => (square === 1 ? BOMB : CLOSED))
+        .join(SQUARE_SEPARATOR) +
+      SQUARE_SEPARATOR
     );
   }
 
