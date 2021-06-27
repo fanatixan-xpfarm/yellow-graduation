ff56f86b995c57a1617f6d569c28e01c9c74c379
Attila-Fejer <attila.fejer@ibm.com>
2021-06-27 20:42:31 +0200

    feat(US2/UAT2.1): green

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,24 @@
 class Minesweeper {
+  constructor(board) {
+    this.board = board;
+  }
   print() {
     if (!this.stepped) {
       return '+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+';
     }
 
-    return '+-+-+-+\n| | | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| | | |\n+-+-+-+';
+    return (
+      '+-+-+-+\n' +
+      this.board
+        .map(
+          (row) =>
+            '|' +
+            row.map((square) => (square === 1 ? 'X' : ' ')).join('|') +
+            '|'
+        )
+        .join('\n+-+-+-+\n') +
+      '\n+-+-+-+'
+    );
   }
 
   step() {
